488a46bcf8e05275fc079bd54fa217b75d5ee74a
ayako world <ay14110mpda@gmail.com>
2019-01-28 18:07:53 +0900

    feature: 動線作成

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import HelloWorld from '@/components/HelloWorld';
+import Home from '@/components/Home';
+import EntryForm from '@/components/EntryForm';
+import EntryComplete from '@/components/EntryComplete';
+import Gacha from '@/components/Gacha';
+import Result from '@/components/Result';
 
 Vue.use(Router);
 
@@ -8,8 +12,28 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld,
+      name: 'Home',
+      component: Home,
+    },
+    {
+      path: '/EntryForm',
+      name: 'EntryForm',
+      component: EntryForm,
+    },
+    {
+      path: '/EntryComplete',
+      name: 'EntryComplete',
+      component: EntryComplete,
+    },
+    {
+      path: '/Gacha',
+      name: 'Gacha',
+      component: Gacha,
+    },
+    {
+      path: '/Result',
+      name: 'Result',
+      component: Result,
     },
   ],
 });
