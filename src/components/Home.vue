0a139a76dbf50fbf710b84e715889c0a5fc85ae5
ayako world <ay14110mpda@gmail.com>
2019-01-30 11:26:07 +0900

    #1 feature: add toppage contents

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,37 +1,70 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>Home</h2>
-    <router-link to="/EntryForm">EntryForm</router-link>
-    <router-link to="/Gacha">Gacha</router-link>
+  <div class="page-wrapper">
+    <div class="contents">
+      <div>
+        <img src="http://placehold.jp/250x150.png" alt="ロゴ">
+      </div>
+      <div class="about-vg">
+        <h2 class="title">what's バレンタインガチャ</h2>
+        <p class="description">バレンタインガチャの概要のテキストが入るよ。ここはプロの心の叫びを思いっきり書きたいね。<br>エモい文章入れてこう！</p>
+      </div>
+      <router-link to="/EntryForm" class="link-area">
+        <div class="box--button">
+          <div>チョコが欲しいあなた</div>
+          <div>Check！</div>
+        </div>
+      </router-link>
+      <router-link to="/Gacha" class="link-area">
+        <div class="box--button">
+          <div>チョコをあげたいあなた</div>
+          <div>Check！</div>
+        </div>
+      </router-link>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-    };
-  },
 };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h1, h2 {
+h2 {
+  font-size: inherit;
   font-weight: normal;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+
+.contents {
+  text-align: center;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+
+.about-vg {
+  padding-bottom: 20px;
 }
-a {
-  color: #42b983;
+
+.title {
+  padding: 20px 0;
+}
+
+.description {
+  line-height: 1.8;
+}
+
+.link-area {
+  text-decoration: none;
+  color: #2c3e50;
+  margin: 0 auto;
+}
+
+.box--button {
+  border: 1px solid #777;
+  width: 200px;
+  margin: 0 auto 40px;
+  padding: 20px;
+  text-align: left;
+  line-height: 1.8;
 }
 </style>
