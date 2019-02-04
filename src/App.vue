23c3126f15e4789228ba67f6c19d29adbcbe594b
aaaaayako <ay14110mpda@gmail.com>
2019-02-04 09:43:20 +0900

    Revert "Feature/static result"

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,13 @@
 <template>
   <div id="app">
+    <img src="./assets/logo.png">
     <router-view/>
   </div>
 </template>
 
 <script>
-import 'normalize.css';
-import store from './store/';
-
 export default {
   name: 'App',
-  store,
 };
 </script>
 
@@ -19,22 +16,8 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  color: #2c3e50;
-}
-
-.page-wrapper {
-  padding: 20px;
-  max-width: 375px;
-  margin: 50px auto;
-}
-
-.contents {
   text-align: center;
-}
-
-.link-area {
-  text-decoration: none;
   color: #2c3e50;
-  margin: 0 auto;
+  margin-top: 60px;
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,6 @@
 import Vue from 'vue';
 import Router from 'vue-router';
-import Home from '@/components/Home';
-import EntryForm from '@/components/EntryForm';
-import Gacha from '@/components/Gacha';
-import Result from '@/components/Result';
+import HelloWorld from '@/components/HelloWorld';
 
 Vue.use(Router);
 
@@ -11,23 +8,8 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'Home',
-      component: Home,
-    },
-    {
-      path: '/EntryForm',
-      name: 'EntryForm',
-      component: EntryForm,
-    },
-    {
-      path: '/Gacha',
-      name: 'Gacha',
-      component: Gacha,
-    },
-    {
-      path: '/Result',
-      name: 'Result',
-      component: Result,
+      name: 'HelloWorld',
+      component: HelloWorld,
     },
   ],
 });
