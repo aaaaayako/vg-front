893e73fbc2f0542931a5581464fb23f298a1b53a
aaaaayako <ay14110mpda@gmail.com>
2019-02-17 19:05:32 +0900

    add: Reflect the design to the entry page

--- src/components/EntryForm.vue
+++ src/components/EntryForm.vue
@@ -1,12 +1,19 @@
 <template>
-  <div class="page-wrapper">
-    <div class="contents">
-      <input @input="doUpdate" class="input-name" placeholder="本名入れてね">
-      <router-link to="/Gacha" class="link-area">
-        <div class="entry-button">
-          ガチャを回す
-        </div>
-      </router-link>
+  <div class="entry-form-container contents">
+    <div class="contents-container">
+      <div class="entry-form-wrapper">
+        <h1>
+          <img src="../assets/choco-presenter-title.png" alt="チョコをあげたい！あなた" width="240" height="106">
+        </h1>
+        <p class="choco-presenter-desc">とっても親切なあなた・・・<br>あなたの事を教えてね♡</p>
+        <label for="full-name">フルネーム</label>
+        <input @input="doUpdate" id="full-name" class="input-name" placeholder="ex) 荒川涼太">
+        <router-link to="/Gacha" class="link-area">
+          <div class="entry-button">
+            ガチャを回す
+          </div>
+        </router-link>
+      </div>
     </div>
   </div>
 </template>
@@ -29,22 +36,44 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-.entry-button {
-  border: 1px solid #2c3e50;;
-  width: 250px;
+.entry-form-container {
   margin: 0 auto;
-  padding: 20px;
-  text-align: center;
-  border-radius: 4px;
+  color: #412929;
+}
+
+.entry-form-wrapper {
+  padding: 60px;
+  margin: 0 auto;
+}
+
+.choco-presenter-desc {
+  line-height: 1.7;
+  padding: 44px 0;
+}
+
+.entry-button {
+  background-color: #8e5d4b;
+  border-radius: 12px;
+  font-size: 16px;
+  color: #fff;
+  font-weight: bold;
+  width: 314px;
+  height: 46px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 }
 
 .input-name {
-  margin: 20px 0;
+  margin: 12px 0 20px;
   padding: 0 20px;
   font-size: 14px;
-  width: 250px;
-  height: 40px;
-  border: 2px solid #2c3e50;;
-  border-radius: 4px;
+  width: 314px;
+  height: 46px;
+  border: 1px solid #b4b4b4;
+  border-radius: 12px;
+  text-align: center;
+  box-sizing: border-box;
 }
 </style>
